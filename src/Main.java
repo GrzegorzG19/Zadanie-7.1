6de0d0eb96fe832f7380e0aa96a9c0825447ce43
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-23 16:22:07 +0200

    Zada1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] tab = new String[5];
+
+        System.out.println("Wpisz 5 imion:");
+
+        for (int i = 0;i<5;i++){
+
+            tab[i]= scan.nextLine();
+        }
+        for (int i = 4;i>=0;i--){
+            System.out.printf("Cześć %s:\n",tab[i]);
+
+        }
+
+
+    }
+}
